81469682ed2c54dc13f0b0948a0dc6859c44e84e
Andrew <shifted6@gmail.com>
2020-03-18 17:16:32 -0700

    Initial file creation. Writes to file.

---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("To-do list:");
+            Console.WriteLine("1. Add item");
+
+            string userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                WriteToAFile("../../../List.txt");
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection. Please enter the number of one of the above options.");
+                Main();
+            }
+        }
+        static void WriteToAFile(string path)
+        {
+            File.WriteAllText(path, "test");
+        }
+    }
+}
