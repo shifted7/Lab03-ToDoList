8f282d85b1c21c70364e7943da4f0077ab08209e
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-19 10:53:10 -0700

    Added exception handling to methods, added summary comments.

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -7,43 +7,85 @@ namespace ToDoList
     {
         static void Main()
         {
-            Console.WriteLine("Quarantine To-do list:");
-            Console.WriteLine("1. Add item");
-            Console.WriteLine("2. View Items");
-            Console.WriteLine("3. Update Items");
-            Console.WriteLine("4. Delete Item");
-
-            string userInput = Console.ReadLine();
-            if (userInput == "1")
+            try
             {
-                WriteToAFile("../../../List.txt");
+                StartSequence();
             }
-            else if (userInput == "2")
+            catch (Exception e)
             {
-                ReadAllLines("../../../List.txt");
+                Console.WriteLine(e.Message);
             }
-            else if (userInput == "3")
+            finally
             {
-                UpdateList("../../../List.txt");
-            }
-            else if (userInput == "4") {
-                Console.WriteLine("Enter the index of the item you want to delete:");
-                int lineToDelete = Convert.ToInt32(Console.ReadLine());
-                DeleteItem("../../../List.txt", lineToDelete);
-            }
-            else
-            {
-                Console.WriteLine("Invalid selection. Please enter the number of one of the above options.");
-                Main();
+                Console.WriteLine("Thanks for self isolating!");
             }
         }
+
+        public static void StartSequence()
+        {
+            try
+            {
+                Console.WriteLine("Quarantine To-do list:");
+                Console.WriteLine("1. Add item");
+                Console.WriteLine("2. View Items");
+                Console.WriteLine("3. Update Items");
+                Console.WriteLine("4. Delete Item");
+                Console.WriteLine("5. Exit Application");
+
+                string userInput = Console.ReadLine();
+                if (userInput == "1")
+                {
+                    WriteToAFile("../../../List.txt");
+                }
+                else if (userInput == "2")
+                {
+                    ReadAllLines("../../../List.txt");
+                }
+                else if (userInput == "3")
+                {
+                    UpdateList("../../../List.txt");
+                }
+                else if (userInput == "4")
+                {
+                    Console.WriteLine("Enter the index of the item you want to delete:");
+                    int lineToDelete = Convert.ToInt32(Console.ReadLine());
+                    DeleteItem("../../../List.txt", lineToDelete);
+                }
+                else if (userInput == "5")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection. Please enter the number of one of the above options.");
+                }
+                StartSequence();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"So this happened: {e.Message}");
+                StartSequence();
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"Why did you do this: {e.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Writes a new to do list item to the file at the end of the file.
+        /// </summary>
+        /// <param name="path">The path of the file to write to.</param>
         public static void WriteToAFile(string path)
         {
             try
             {
                 Console.WriteLine("What do you want to add to this list?");
                 string[] input = { Console.ReadLine() };
-                // File.WriteAllText(path, ""); 
+                if (input == null)
+                {
+                    throw new Exception("Cannot add empty dreams to your list.");
+                }
                 File.AppendAllLines(path, input);
             }
             catch (FileNotFoundException)
@@ -65,24 +107,40 @@ namespace ToDoList
             }
         }
 
-        //update the list
+        /// <summary>
+        /// Update an item on the to do list.
+        /// </summary>
+        /// <param name="path">The path of the file to write to.</param>
         public static void UpdateList(string path)
         {
-            string[] myList = File.ReadAllLines(path);
-
-            for (int i = 0; i < myList.Length; i++)
+            try
             {
-                Console.WriteLine($"{i + 1}. {myList[i]} ");
+                string[] myList = File.ReadAllLines(path);
+
+                for (int i = 0; i < myList.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {myList[i]} ");
+                }
+                Console.WriteLine("What do you want to update?");
+                string index = Console.ReadLine();
+                int indexParse = Convert.ToInt32(index);
+                Console.WriteLine("What do you want to change it to?");
+                string input = Console.ReadLine();
+                myList[indexParse - 1] = input;
+                File.WriteAllLines(path, myList);
             }
-            Console.WriteLine("What do you want to update?");
-            string index = Console.ReadLine();
-            int indexParse = Convert.ToInt32(index);
-            Console.WriteLine("What do you want to change it to?");
-            string input = Console.ReadLine();
-            myList[indexParse-1] = input;
-            File.WriteAllLines(path, myList);
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                UpdateList(path);
+            }
         }
 
+        /// <summary>
+        /// Deletes an item from the to do list.
+        /// </summary>
+        /// <param name="path">The path of the file to write to.</param>
+        /// <param name="lineToDelete">Index number of the item to delete.</param>
         public static void DeleteItem(string path, int lineToDelete)
         {
             string[] fileText = File.ReadAllLines(path);
