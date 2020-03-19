9c2ef2cf078478e654b87005bcb6626afc096d12
Jin Kim <jinwoov@gmail.com>
2020-03-18 18:36:45 -0700

    Update is finished and letting the user to update depending on the number of their choice and their input

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -10,6 +10,7 @@ namespace ToDoList
             Console.WriteLine("Quarantine To-do list:");
             Console.WriteLine("1. Add item");
             Console.WriteLine("2. View Items");
+            Console.WriteLine("3. Update Items");
 
             string userInput = Console.ReadLine();
             if (userInput == "1")
@@ -19,6 +20,10 @@ namespace ToDoList
             else if (userInput == "2")
             {
                 ReadAllLines("../../../List.txt");
+            }
+            else if (userInput == "3")
+            {
+                UpdateList("../../../List.txt");
             }
             else
             {
@@ -53,6 +58,25 @@ namespace ToDoList
                 Console.WriteLine(myList[i]);
             }
         }
+
+        //update the list
+        public static void UpdateList(string path)
+        {
+            string[] myList = File.ReadAllLines(path);
+
+            for (int i = 0; i < myList.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {myList[i]} ");
+            }
+            Console.WriteLine("What do you want to update?");
+            string index = Console.ReadLine();
+            int indexParse = Convert.ToInt32(index);
+            Console.WriteLine("What do you want to change it to?");
+            string input = Console.ReadLine();
+            myList[indexParse-1] = input;
+            File.WriteAllLines(path, myList);
+        }
+
 
     }
 }
